b0eb470b08fe0f37e0a1b47a96400f9a79ec6bd7
Ihar Vershal <Ihar_Vershal@epam.com>
2017-10-02 11:48:22 +0300

    fixed pom

--- src/main/java/com/epam/entity/Author.java
+++ src/main/java/com/epam/entity/Author.java
@@ -1,7 +1,5 @@
 package com.epam.entity;
 
-import javax.xml.bind.annotation.XmlElement;
-
 public class Author {
     String name;
     Article article;
@@ -10,21 +8,41 @@ public class Author {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Article getArticle() {
         return article;
     }
 
-    @XmlElement
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    @XmlElement
     public void setArticle(Article article) {
         this.article = article;
     }
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", article=" + article +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (name != null ? !name.equals(author.name) : author.name != null) return false;
+        return article != null ? article.equals(author.article) : author.article == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (article != null ? article.hashCode() : 0);
+        return result;
+    }
 }
